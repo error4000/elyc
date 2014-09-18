6c53b1b5b34a6c2024a62f8f02c617e272d556a2
error4000 <hannu.kreivi@gmail.com>
2014-09-18 15:01:21 -0400

    Ok, 18.9.2014

--- t2/t2/t2.c
+++ t2/t2/t2.c
@@ -10,22 +10,37 @@ int main ( int argc, char *argv[] )
 {
 	FILE *tiedostoeka;
 	FILE *tiedostotoka;
+	char rivi[100];
 
 
-	tiedostoeka=fopen("ekatiedosto.txt","r"),argv[1];
+
+
+	//tiedostoeka=fopen("ekatiedosto.txt","r"),argv[1];
+	tiedostoeka = fopen(argv[1],"r");
 
 	//tarkistetaan loytyyko ekatiedostoa
 	if (tiedostoeka==NULL) {
-	printf("File [ekatiedosto.txt] not found!");
+	printf("File 'tiedostoeka' not found!");
 	}
 	//Jos loytyy, jatketaan eteenpain
 	else {
 
-	tiedostotoka=fopen("tokatiedosto.txt","w"),argv[2];
+	//tiedostotoka=fopen("tokatiedosto.txt","w"),argv[2];
+	tiedostotoka=fopen(argv[2],"w");
+	
+
+	while(!feof(tiedostoeka)) {
+	fgets(rivi, 100, tiedostoeka);
+
+	fputs(rivi,tiedostotoka);
+	fclose(tiedostoeka);
+	fclose(tiedostotoka);
 	}
+}
+
+}	
 
 
-	}
 
 
 

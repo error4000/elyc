8400ba41bba1b5284d95f29e79743e13c4254dc3
error4000 <hannu.kreivi@gmail.com>
2014-09-19 01:52:26 -0400

    Kesken

---
+++ t3/t3/t3.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//t3.c | Hannu Kreivi, TYO14SOHJ
+//Kirjoita ohjelma nimelta laskemerkit, joka laskee annetusta tiedostoista merkkien  maaran.
+//Tiedosto annetaan komentorivilta. Merkeiksi lasketaan kaikki merkit myos valilyonnit ja rivinvaihtomerkit.
+

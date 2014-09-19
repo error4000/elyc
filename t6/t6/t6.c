9abfa72d1bda425b7269591da97c56a4b782b21e
error4000 <hannu.kreivi@gmail.com>
2014-09-19 06:49:37 -0400

    Kesken

---
+++ t6/t6/t6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+//t6.c | Hannu Kreivi, TYO14SOHJ
+//Funktio strchr etsii parametrin merkki ensimmäistä esiintymää parametrin jono osoittamasta merkkitaulukosta. strchr on määritelty kirjastossa string.h. Funktio palauttaa arvon NULL, jos etsittyä merkkiä ei löydy mainitusta jonosta, 
+//muuten se palauttaa etsityn merkkijonon alkion osoitteen.
+
+int main (void)
+
+{
+	char merkki[8];
+	char *p;
+
+
+
+}

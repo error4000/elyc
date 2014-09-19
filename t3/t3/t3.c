1bada504ecbd4def3e0c184341b00c2eee4d3de6
error4000 <hannu.kreivi@gmail.com>
2014-09-19 06:36:24 -0400

    Ok, 19.9.2014

--- t3/t3/t3.c
+++ t3/t3/t3.c
@@ -1,5 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <string.h> //Merkkijono funkkarit
 //t3.c | Hannu Kreivi, TYO14SOHJ
-//Kirjoita ohjelma nimelta laskemerkit, joka laskee annetusta tiedostoista merkkien  maaran.
-//Tiedosto annetaan komentorivilta. Merkeiksi lasketaan kaikki merkit myos valilyonnit ja rivinvaihtomerkit.
+//Kirjoita ohjelma nimeltä laskemerkit, joka laskee annetusta tiedostosta merkkien määrän. 
+//Tiedostonimi annetaan komentoriviltä. Merkeiksi lasketaan kaikki merkit myös välilyönnit ja rivinvaihtomerkit. 
 
+int main ( int argc, char *argv[] )
+
+{
+	FILE *tiedosto;
+	char rivi[200];
+	char str[100];
+	int len;
+	
+
+	//Avataan tiedosto lukemista varten
+	tiedosto = fopen(argv[1],"r"); 
+	//tarkistetaan loytyyko tiedostoa
+	if (tiedosto==NULL) {
+	printf("File not found!\n");
+	}
+	//Jos loytyy, jatketaan eteenpain
+	else {
+
+	while(!feof(tiedosto)) {
+	fgets(rivi, 200, tiedosto);
+
+	strcpy(str, rivi);
+	len = strlen(str);
+
+	printf("Merkkien maara annetun tiedoston rivissa on %d\n", len);
+
+   return(0);
+
+	}
+
+	}
+
+
+}	

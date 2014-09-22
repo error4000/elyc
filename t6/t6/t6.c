1efcd6e1d176be9fbf4f829427d7ab0656d86927
error4000 <hannu.kreivi@gmail.com>
2014-09-22 01:42:02 -0400

    Ok,22.9.2014

--- t6/t6/t6.c
+++ t6/t6/t6.c
@@ -4,12 +4,19 @@
 //Funktio strchr etsii parametrin merkki ensimmäistä esiintymää parametrin jono osoittamasta merkkitaulukosta. strchr on määritelty kirjastossa string.h. Funktio palauttaa arvon NULL, jos etsittyä merkkiä ei löydy mainitusta jonosta, 
 //muuten se palauttaa etsityn merkkijonon alkion osoitteen.
 
-int main (void)
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
 {
-	char merkki[8];
+	char merkki;
+	char merkkitaulu[] = "taateli";
 	char *p;
 
+	printf("anna kirjain: ");
+	scanf("%c", &merkki);
 
+	p = strchr(merkkitaulu, merkki);
 
+	printf("p:n arvo: %i\n", p);
 }
